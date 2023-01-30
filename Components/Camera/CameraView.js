88c9417a68a14e9027d2467e44992365920811a7
Justin <47122570+justintricate@users.noreply.github.com>
2023-01-30 10:43:49 -0600

    update cameraview.js

--- Components/Camera/CameraView.js
+++ Components/Camera/CameraView.js
@@ -20,7 +20,7 @@ export default function CameraView() {
   if (!permission.granted) {
     // Camera permissions are not granted yet
     return (
-      <View style={{flex: 1, justifyContent: "center"}>
+      <View style={{flex: 1, justifyContent: "center"}}>
         <Text style={{ textAlign: "center" }}>
           We need your permission to show the camera
         </Text>
