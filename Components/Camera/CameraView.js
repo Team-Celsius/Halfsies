d91500ad8b7eeef31c131229e8b5230fd29c2865
sking11 <114305188+sking11@users.noreply.github.com>
2023-01-31 12:18:01 -0700

    Revert "restyling CameraView, in progress"
    This reverts commit 2ab82b11bcf92bc0c976342ec92fd14fdca15e71.

--- Components/Camera/CameraView.js
+++ Components/Camera/CameraView.js
@@ -1,11 +1,13 @@
-import { Button, TouchableOpacity, View, Image, } from "react-native";
-import { useState, useRef } from "react";
-import * as ImagePicker from "expo-image-picker";
 import { Camera, CameraType } from "expo-camera";
-import { Text, Spacer, VStack } from "native-base";
-import { MaterialCommunityIcons, Feather, FontAwesome5 } from "@expo/vector-icons";
+import { useState, useRef } from "react";
+import { Button, Text, TouchableOpacity, View, Image } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function CameraView() {
+  const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [capturedImage, setCapturedImage] = useState(null);
   const cameraRef = useRef(null);
@@ -17,15 +19,13 @@ export default function CameraView() {
 
   if (!permission.granted) {
     // Camera permissions are not granted yet
-    return ( 
-      <VStack w="100%" h="80%">
-        <Spacer />
-        <Text alignSelf="center" fontSize="16">
-          We need your permission to access the camera
+    return (
+      <View style={{flex: 1, justifyContent: "center"}}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
         </Text>
-        <Button onPress={requestPermission} title="Allow camera access" border=""/>
-        <Spacer />
-      </VStack>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
     );
   }
 
@@ -113,26 +113,70 @@ export default function CameraView() {
           </View>
         </View>
       ) : (
-          <VStack w="100%" h="100%">
-            <Camera ref={cameraRef} flashMode="on" >
-              <Text>
-                Hold your device directly above the receipt. For optimal image
-                processing, flash will be used.
-              </Text>
-              <TouchableOpacity onPress={takePhoto}>
-                <MaterialCommunityIcons
-                  name="camera-iris"
-                  size={75}
-                  color="white"
-                  />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={pickImage}>
-                  <Feather name="upload" size={24} color="white" />
-                  <Text>Upload</Text>
-              </TouchableOpacity>
-            </Camera>
-          </VStack>
-              
+        <Camera style={{ flex: 1 }} type={type} ref={cameraRef} flashMode="on">
+          <View style={{ flex: 1, alignItems: "center", top: 65 }}>
+            <Text
+              style={{
+                fontSize: 18,
+                color: "white",
+                textAlign: "center",
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                width: "100%",
+              }}
+            >
+              Hold your device directly above the receipt. For optimal image
+              processing, flash will be used.
+            </Text>
+          </View>
+
+          <View
+            style={{
+              bottom: 70,
+              justifyContent: "center",
+              alignItems: "center",
+              position: "absolute",
+              width: "100%",
+              height: 90,
+            }}
+          >
+            <TouchableOpacity
+              style={{ flex: 1, alignItems: "center" }}
+              onPress={takePhoto}
+            >
+              <MaterialCommunityIcons
+                name="camera-iris"
+                size={75}
+                color="white"
+              />
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              bottom: 0,
+              right: -200,
+              alignItems: "flex-end",
+              justifyContent: "flex-end",
+              flex: 1,
+              position: "relative",
+              width: "50%",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                position: "absolute",
+                bottom: 90,
+                right: 20,
+                width: "50%",
+              }}
+              onPress={pickImage}
+            >
+              <View style={{ alignItems: "center" }}>
+                <Feather name="upload" size={24} color="white" />
+                <Text style={{ color: "white" }}>Upload</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </Camera>
       )}
     </View>
   );
