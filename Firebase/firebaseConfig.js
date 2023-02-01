87a32d773f30db8e7b5a780c42cb8597029eb154
jason <potvinj@gmail.com>
2023-02-01 12:56:09 -0500

    add firebase config

---
+++ Firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFXaDjUdibz94CNra1AQVSdAL8Dlhu2ZY",
+  authDomain: "halfsies-5d250.firebaseapp.com",
+  projectId: "halfsies-5d250",
+  storageBucket: "halfsies-5d250.appspot.com",
+  messagingSenderId: "1013068961806",
+  appId: "1:1013068961806:web:bf6969835f14cc85fb845f",
+};
+
+const app = initializeApp(firebaseConfig);
