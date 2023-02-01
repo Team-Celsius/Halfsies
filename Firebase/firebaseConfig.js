c3dfa97bf4206d4af35213b83e5ee15651be689e
jason <potvinj@gmail.com>
2023-02-01 17:23:01 -0500

    add auth and db

--- Firebase/firebaseConfig.js
+++ Firebase/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCFXaDjUdibz94CNra1AQVSdAL8Dlhu2ZY",
@@ -13,3 +14,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
+const db = getFirestore();
+
+export { auth, db };
