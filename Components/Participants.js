a7f5e43b78b01a7e6cb62b61263f4ee93acd6212
sking11 <114305188+sking11@users.noreply.github.com>
2023-02-02 14:50:51 -0700

    added function to populate favorites section. Also added avatar color so it persists between favorites section and alphabetical section

--- Components/Participants.js
+++ Components/Participants.js
@@ -10,24 +10,66 @@ export default function Participants() {
     {
       initials: "SK",
       paymentHandle: "@therealsteven",
-      name: "Steven King"
+      name: "Steven King",
+      favoritesCounter: 1,
+      avatarColor: randomColor()
     },
     {
       initials: "JW",
       paymentHandle: "@justinw",
-      name: "Justin Wooley"
+      name: "Justin Wooley",
+      favoritesCounter: 2,
+      avatarColor: randomColor()
     },
     {
       initials: "JP",
       paymentHandle: "@jasonp",
-      name: "Jason Potvin"
+      name: "Jason Potvin",
+      favoritesCounter: 4,
+      avatarColor: randomColor()
     },
     {
       initials: "MT",
       paymentHandle: "@michaeltheboss",
-      name: "Michael Timo"
+      name: "Michael Timo",
+      favoritesCounter: 3,
+      avatarColor: randomColor()
     }
   ]
+
+  function FavoriteFriendsSection(props) {
+    let friends = props.friends
+    let favorites = []
+
+    for(let i = 0; i < 4; ++i) {
+      let currentHighest = friends[0]
+      let currentHighestIndex = 0
+
+      friends.map(friend => {
+        if(friends[friends.indexOf(friend) + 1]) {
+          let nextFriend = friends[friends.indexOf(friend) + 1]
+          if(currentHighest.favoritesCounter <= nextFriend.favoritesCounter) {
+            currentHighest = nextFriend
+            currentHighestIndex = friends.indexOf(nextFriend)
+          }
+        }
+      })
+      favorites.push(currentHighest)
+      friends[currentHighestIndex].favoritesCounter = 0
+    }
+    return(<>
+      {favorites.map(friend => {
+        return(<>
+          <HStack space="3" m="1"> 
+            <Avatar bg={friend.avatarColor}>{friend.initials}</Avatar>
+            <VStack> 
+              <Text>{friend.paymentHandle}</Text>
+              <Text pl="3">{friend.name}</Text>
+            </VStack>
+          </HStack>
+        </>)
+      })}
+    </>)}
 
   function AlphabeticalFriendsSection(props) {
     let alphabet = props.alphabet
@@ -40,7 +82,7 @@ export default function Participants() {
             if(friend.name[0] === letter) {
               return (<>
                 <HStack space="3" m="1"> 
-                  <Avatar bg={randomColor()}>{friend.initials}</Avatar>
+                  <Avatar bg={friend.avatarColor}>{friend.initials}</Avatar>
                   <VStack> 
                     <Text>{friend.paymentHandle}</Text>
                     <Text pl="3">{friend.name}</Text>
@@ -82,43 +124,12 @@ export default function Participants() {
       </HStack>
 
       <HStack flex={1}>
+
         <ScrollView>
           <VStack space="4" pl="3">
-
-            {/* Favorite contacts section */}
             <Text>Favorites</Text>
-            <HStack space="3"> 
-              <Avatar bg="green.500">JW</Avatar>
-              <VStack> 
-                <Text>@justinw</Text>
-                <Text pl="3">Justin Wooley</Text>
-              </VStack>
-            </HStack>
-            <HStack space="3"> 
-              <Avatar bg="blue.500">JP</Avatar>
-              <VStack> 
-                <Text>@jasonp</Text>
-                <Text pl="3">Jason Potvin</Text>
-              </VStack>
-            </HStack>
-            <HStack space="3"> 
-              <Avatar bg="red.500">MT</Avatar>
-              <VStack> 
-                <Text>@michaeltheboss</Text>
-                <Text pl="3">Michael Timo</Text>
-              </VStack>
-            </HStack>
-            <HStack space="3"> 
-              <Avatar bg="yellow.500">SK</Avatar>
-              <VStack> 
-                <Text>@therealsteven</Text>
-                <Text pl="3">Steven King</Text>
-              </VStack>
-            </HStack>
-            <Divider w="100%" alignSelf="center"/>
-
+            <FavoriteFriendsSection friends={friends}/>
             <AlphabeticalFriendsSection alphabet={alphabet} friends={friends}/>
-
           </VStack>
         </ScrollView>
 
@@ -127,6 +138,7 @@ export default function Participants() {
         </VStack>
 
       </HStack>
+
     </VStack></>
   )
 }
