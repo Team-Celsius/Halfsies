9786bd24ba7d754fbb40d387b47923dc28de09de
jason <potvinj@gmail.com>
2023-02-02 15:43:28 -0500

    create form to log in

---
+++ Components/Login/LoginForm.js
@@ -0,0 +1,86 @@
+import { Image, ZStack, VStack, Input, Spacer, Pressable } from "native-base";
+import { useForm, Controller } from "react-hook-form";
+import { Text, View, TextInput, Button, Alert } from "react-native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../Firebase/firebaseConfig";
+
+export default function LoginForm() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    signInWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log("user", user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
+  return (
+    <View>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+          pattern: /^\S+@\S+$/i,
+        }}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            textAlign="center"
+            borderWidth="3"
+            borderColor="violet.800"
+            variant="rounded"
+            backgroundColor="white"
+            color="violet.800"
+            p="5"
+            m="2"
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="email"
+      />
+      {errors.email && <Text>Email is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+          minLength: 6,
+          maxLength: 100,
+        }}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            textAlign="center"
+            borderWidth="3"
+            borderColor="violet.800"
+            variant="rounded"
+            backgroundColor="white"
+            color="violet.800"
+            p="5"
+            m="2"
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="password"
+      />
+      {errors.password && <Text>Password is required.</Text>}
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    </View>
+  );
+}
