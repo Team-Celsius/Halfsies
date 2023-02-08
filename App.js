9542796693228af2a1991eec981831daee7e3abb
sking11 <114305188+sking11@users.noreply.github.com>
2023-02-07 22:03:34 -0700

    assign items page can now add user to item, can tap avatar under item to delete, updates each participants items array to include what they had, still need to add functionality for all button and + person

--- App.js
+++ App.js
@@ -28,137 +28,16 @@ export default function App() {
   });
 
   const [participants, setParticipants] = useState([
-    {
-      initials: "SK",
-      name: "Steven King",
-      email: "email",
-      numPaymentRequests: 100,
-      avatarColor: randomColor(),
-      selected: false,
-    },
-    {
-      initials: "JW",
-      name: "Justin Wooley",
-      email: "email",
-      numPaymentRequests: 2000,
-      avatarColor: randomColor(),
-      selected: false,
-    },
-    {
-      initials: "JP",
-      name: "Jason Potvin",
-      email: "email",
-      numPaymentRequests: 4,
-      avatarColor: randomColor(),
-      selected: false,
-    },
-    {
-      initials: "MT",
-      name: "Michael Timo",
-      email: "email",
-      numPaymentRequests: 8,
-      avatarColor: randomColor(),
-      selected: false,
-    },
-    {
-      initials: "AS",
-      name: "Andy Smith",
-      email: "email",
-      numPaymentRequests: 0,
-      avatarColor: randomColor(),
-      selected: false,
-    },
-  ]);
-  // const [friends, setFriends] = useState([
-  //   {
-  //     initials: "SK",
-  //     name: "Steven King",
-  //     email: "email",
-  //     numPaymentRequests: 100,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  //   {
-  //     initials: "JW",
-  //     name: "Justin Wooley",
-  //     email: "email",
-  //     numPaymentRequests: 2000,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  //   {
-  //     initials: "JP",
-  //     name: "Jason Potvin",
-  //     email: "email",
-  //     numPaymentRequests: 4,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  //   {
-  //     initials: "MT",
-  //     name: "Michael Timo",
-  //     email: "email",
-  //     numPaymentRequests: 8,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  //   {
-  //     initials: "AS",
-  //     name: "Andy Smith",
-  //     email: "email",
-  //     numPaymentRequests: 0,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  //   {
-  //     initials: "LS",
-  //     name: "Lauren Smith",
-  //     email: "email",
-  //     numPaymentRequests: 0,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  //   {
-  //     initials: "AC",
-  //     name: "Ashley Campbell",
-  //     email: "email",
-  //     numPaymentRequests: 0,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  //   {
-  //     initials: "RM",
-  //     name: "Rich Merril",
-  //     email: "email",
-  //     numPaymentRequests: 0,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  //   {
-  //     initials: "JL",
-  //     name: "Jeff Lincoln",
-  //     email: "email",
-  //     numPaymentRequests: 0,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  //   {
-  //     initials: "AJ",
-  //     name: "Alexander Joseph",
-  //     email: "email",
-  //     numPaymentRequests: 0,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  //   {
-  //     initials: "ZS",
-  //     name: "Zachary Smith",
-  //     email: "email",
-  //     numPaymentRequests: 0,
-  //     avatarColor: randomColor(),
-  //     selected: false,
-  //   },
-  // ]);
+    {userId: '',
+    initials: '',
+    name: '',
+    email: '',
+    numPaymentRequests: 0,
+    avatarColor: randomColor(),
+    selected: false,
+    items: []}
+  ])
+
   return (
     <NavigationContainer>
       <NativeBaseProvider>
