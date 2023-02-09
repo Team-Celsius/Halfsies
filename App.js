3cacfd847a1748aa13261d1474f5a3c6b4ce884c
Justin <47122570+justintricate@users.noreply.github.com>
2023-02-08 22:32:31 -0600

    minor styling enhancements (login view, pg titles)

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import "react-native-gesture-handler";
-import { NativeBaseProvider, Flex } from "native-base";
+import { NativeBaseProvider } from "native-base";
 import LoginPage from "./Components/Login/LoginPage";
 import Participants from "./Components/Participants";
 import AssignItems from "./Components/AssignItems";
 import CameraView from "./Components/Camera/CameraView";
 import BalancePage from "./Components/BalancePage";
-
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./Firebase/firebaseConfig";
 import { useState } from "react";
@@ -16,43 +15,53 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
+	const [isLoggedIn, setLoggedIn] = useState(false);
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setLoggedIn(true);
-      const uid = user.uid;
-    } else {
-      setLoggedIn(false);
-    }
-  });
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			setLoggedIn(true);
+			const uid = user.uid;
+		} else {
+			setLoggedIn(false);
+		}
+	});
 
-  const [participants, setParticipants] = useState([
-    {userId: '',
-    initials: '',
-    name: '',
-    email: '',
-    numPaymentRequests: 0,
-    avatarColor: randomColor(),
-    selected: false,
-    items: []}
-  ])
+	const [participants, setParticipants] = useState([
+		{
+			userId: "",
+			initials: "",
+			name: "",
+			email: "",
+			numPaymentRequests: 0,
+			avatarColor: randomColor(),
+			selected: false,
+			items: [],
+		},
+	]);
 
-  return (
-    <NavigationContainer>
-      <NativeBaseProvider>
-        <Stack.Navigator initialRouteName="LogIn">
-          <Stack.Screen
-            name="LogIn"
-            component={LoginPage}
-            options={{ title: "Log In" }}
-          />
-          <Stack.Screen name="Camera" component={CameraView} />
-          <Stack.Screen name="Participants" component={Participants} />
-          <Stack.Screen name="AssignItems" component={AssignItems} />
-          <Stack.Screen name="BalancePage" component={BalancePage} />
-        </Stack.Navigator>
-      </NativeBaseProvider>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<NativeBaseProvider>
+				<Stack.Navigator initialRouteName="LogIn">
+					<Stack.Screen
+						name="LogIn"
+						component={LoginPage}
+						options={{ title: "Log In" }}
+					/>
+					<Stack.Screen name="Camera" component={CameraView} />
+					<Stack.Screen
+						name="Participants"
+						component={Participants}
+						options={{ title: "Who are we splitting with?" }}
+					/>
+					<Stack.Screen
+						name="AssignItems"
+						component={AssignItems}
+						options={{ title: "Assign Items" }}
+					/>
+					<Stack.Screen name="BalancePage" component={BalancePage} />
+				</Stack.Navigator>
+			</NativeBaseProvider>
+		</NavigationContainer>
+	);
 }
