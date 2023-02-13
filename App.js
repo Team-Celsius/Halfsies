a610957547440edff61d2f45d1454157d8301325
sking11 <114305188+sking11@users.noreply.github.com>
2023-02-12 17:50:52 -0700

    fixed styling to use nativeBase instead of react native. fixed bug with keyboard avoiding view. removed styling attributes that were not doing anything. Made login/signup clickable on only the underlined texted.

--- App.js
+++ App.js
@@ -1,59 +1,42 @@
-import "react-native-gesture-handler";
-import { NativeBaseProvider } from "native-base";
-import LoginPage from "./Components/Login/LoginPage";
-import Participants from "./Components/Participants";
-import AssignItems from "./Components/AssignItems";
-import CameraView from "./Components/Camera/CameraView";
-import BalancePage from "./Components/BalancePage";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./Firebase/firebaseConfig";
-import { useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import "intl";
-import "intl/locale-data/jsonp/en";
-import Summary from "./Components/Summary";
-const Stack = createNativeStackNavigator();
+import 'react-native-gesture-handler'
+import { NativeBaseProvider } from 'native-base'
+import LoginPage from './Components/Login/LoginPage'
+import Participants from './Components/Participants'
+import AssignItems from './Components/AssignItems'
+import CameraView from './Components/Camera/CameraView'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './Firebase/firebaseConfig'
+import { useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import 'intl'
+import 'intl/locale-data/jsonp/en'
+import Summary from './Components/Summary'
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
+	const [isLoggedIn, setLoggedIn] = useState(false)
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setLoggedIn(true);
-      const uid = user.uid;
-    } else {
-      setLoggedIn(false);
-    }
-  });
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			setLoggedIn(true)
+			const uid = user.uid
+		} else {
+			setLoggedIn(false)
+		}
+	})
 
-  return (
-    <NavigationContainer>
-      <NativeBaseProvider>
-        <Stack.Navigator initialRouteName="LogIn">
-          <Stack.Screen
-            name="LogIn"
-            component={LoginPage}
-            options={{ title: "Log In", headerShown: false }}
-          />
-          <Stack.Screen
-            name="Camera"
-            component={CameraView}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Participants"
-            component={Participants}
-            options={{ title: "Who are we splitting with?" }}
-          />
-          <Stack.Screen
-            name="AssignItems"
-            component={AssignItems}
-            options={{ title: "Assign Items" }}
-          />
-          <Stack.Screen name="Summary" component={Summary} />
-        </Stack.Navigator>
-      </NativeBaseProvider>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<NativeBaseProvider>
+				<Stack.Navigator initialRouteName='LogIn'>
+					<Stack.Screen name='LogIn' component={LoginPage} options={{ title: 'Log In', headerShown: false }} />
+					<Stack.Screen name='Camera' component={CameraView} options={{ headerShown: false }} />
+					<Stack.Screen name='Participants' component={Participants} options={{ title: 'Who are we splitting with?' }} />
+					<Stack.Screen name='AssignItems' component={AssignItems} options={{ title: 'Assign Items' }} />
+					<Stack.Screen name='Summary' component={Summary} />
+				</Stack.Navigator>
+			</NativeBaseProvider>
+		</NavigationContainer>
+	)
 }
--- Components/Login/LoginForm.js
+++ Components/Login/LoginForm.js
@@ -1,174 +1,135 @@
-import { Input, Pressable, Text, Modal, Button, ScrollView } from "native-base";
-import { useForm, Controller } from "react-hook-form";
-import { View } from "react-native";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../Firebase/firebaseConfig";
-import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
-import { Feather } from "@expo/vector-icons";
+import { Input, KeyboardAvoidingView, Text, Modal, Button, ScrollView } from 'native-base'
+import { useForm, Controller } from 'react-hook-form'
+import { View } from 'react-native'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../Firebase/firebaseConfig'
+import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
+import { Feather } from '@expo/vector-icons'
 
 export default function LoginForm() {
-  const navigation = useNavigation();
-  const [modalVisible, setModalVisible] = useState(false);
-  const [errorText, setErrorText] = useState("");
-  const [show, setShow] = useState(false);
+	const navigation = useNavigation()
+	const [modalVisible, setModalVisible] = useState(false)
+	const [errorText, setErrorText] = useState('')
+	const [show, setShow] = useState(false)
 
-  const handleClick = () => setShow(!show);
+	const handleClick = () => setShow(!show)
 
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	})
 
-  const onSubmit = (data) => {
-    signInWithEmailAndPassword(auth, data.email.trim(), data.password)
-      .then((userCredential) => {
-        navigation.navigate("Camera");
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        console.log(errorCode);
-        setErrorText(errorTextCreator(errorCode));
-        setModalVisible(true);
-      });
-  };
+	const onSubmit = (data) => {
+		signInWithEmailAndPassword(auth, data.email.trim(), data.password)
+			.then((userCredential) => {
+				navigation.navigate('Camera')
+			})
+			.catch((error) => {
+				const errorCode = error.code
+				console.log(errorCode)
+				setErrorText(errorTextCreator(errorCode))
+				setModalVisible(true)
+			})
+	}
 
-  function errorTextCreator(errorCode) {
-    if (errorCode === "auth/invalid-email") {
-      return "Email is invalid.";
-    } else if (errorCode === "auth/user-not-found") {
-      return "User not found, please try again.";
-    } else if (errorCode === "auth/wrong-password") {
-      return "Wrong password, please try again.";
-    }
-  }
+	function errorTextCreator(errorCode) {
+		if (errorCode === 'auth/invalid-email') {
+			return 'Email is invalid.'
+		} else if (errorCode === 'auth/user-not-found') {
+			return 'User not found, please try again.'
+		} else if (errorCode === 'auth/wrong-password') {
+			return 'Wrong password, please try again.'
+		}
+	}
 
-  function LogInError() {
-    return (
-      <>
-        <Modal isOpen={modalVisible} onClose={setModalVisible} size="sm">
-          <Modal.Content maxH="212">
-            <Modal.CloseButton />
-            <Modal.Header>Error</Modal.Header>
-            <Modal.Body>
-              <ScrollView>
-                <Text>{errorText}</Text>
-              </ScrollView>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button.Group space={2}>
-                <Button
-                  backgroundColor="violet.900"
-                  onPress={() => {
-                    setModalVisible(false);
-                  }}
-                >
-                  Close
-                </Button>
-              </Button.Group>
-            </Modal.Footer>
-          </Modal.Content>
-        </Modal>
-      </>
-    );
-  }
+	function LogInError() {
+		return (
+			<>
+				<Modal isOpen={modalVisible} onClose={setModalVisible} size='sm'>
+					<Modal.Content maxH='212'>
+						<Modal.CloseButton />
+						<Modal.Header>Error</Modal.Header>
+						<Modal.Body>
+							<ScrollView>
+								<Text>{errorText}</Text>
+							</ScrollView>
+						</Modal.Body>
+						<Modal.Footer>
+							<Button.Group space={2}>
+								<Button
+									backgroundColor='violet.800'
+									onPress={() => {
+										setModalVisible(false)
+									}}>
+									Close
+								</Button>
+							</Button.Group>
+						</Modal.Footer>
+					</Modal.Content>
+				</Modal>
+			</>
+		)
+	}
 
-  return (
-    <View>
-      <LogInError />
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-          pattern: /^\S+@\S+/i,
-        }}
-        render={({ field: { onChange, value } }) => (
-          <Input
-            textAlign="center"
-            borderWidth="3"
-            borderColor="violet.900"
-            variant="rounded"
-            backgroundColor="white"
-            color="violet.900"
-            p="5"
-            m="2"
-            placeholder="Email"
-            onChangeText={onChange}
-            value={value}
-          />
-        )}
-        name="email"
-      />
-      {errors.email && (
-        <Text style={{ textAlign: "center" }}>Email is required.</Text>
-      )}
-      <View style={{ width: "100%", alignItems: "center" }}>
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-            minLength: 1,
-            maxLength: 100,
-          }}
-          render={({ field: { onChange, value } }) => (
-            <Input
-              textAlign="center"
-              borderWidth="3"
-              borderColor="violet.900"
-              variant="rounded"
-              backgroundColor="white"
-              color="violet.900"
-              p="5"
-              m="2"
-              placeholder="Password"
-              onChangeText={onChange}
-              value={value}
-              type={show ? "text" : "password"}
-              InputRightElement={
-                <Button
-                  size="xs"
-                  rounded="none"
-                  w="20"
-                  h="full"
-                  backgroundColor="white"
-                  onPress={handleClick}
-                  style={{ marginLeft: "-23%" }}
-                >
-                  {show ? (
-                    <Feather name="eye-off" size={24} color="gray" />
-                  ) : (
-                    <Feather name="eye" size={24} color="gray" />
-                  )}
-                </Button>
-              }
-            />
-          )}
-          name="password"
-        />
-        {errors.password && <Text>Please enter password</Text>}
-      </View>
+	return (
+		<KeyboardAvoidingView behavior='position'>
+			<LogInError />
+			<Controller
+				control={control}
+				rules={{
+					required: true,
+					pattern: /^\S+@\S+/i,
+				}}
+				render={({ field: { onChange, value } }) => <Input textAlign='center' borderWidth='3' borderColor='violet.800' variant='rounded' backgroundColor='white' color='violet.800' p='5' m='2' placeholder='Email' onChangeText={onChange} value={value} />}
+				name='email'
+			/>
+			{errors.email && <Text textAlign='center'>Email is required.</Text>}
+			<View w='100%' alignItems='center'>
+				<Controller
+					control={control}
+					rules={{
+						required: true,
+						minLength: 1,
+						maxLength: 100,
+					}}
+					render={({ field: { onChange, value } }) => (
+						<Input
+							textAlign='center'
+							borderWidth='3'
+							borderColor='violet.800'
+							variant='rounded'
+							backgroundColor='white'
+							color='violet.800'
+							p='5'
+							m='2'
+							placeholder='Password'
+							onChangeText={onChange}
+							value={value}
+							type={show ? 'text' : 'password'}
+							InputRightElement={
+								<Button size='xs' backgroundColor='white' onPress={handleClick} ml='-15%'>
+									{show ? <Feather name='eye-off' size={24} color='gray' /> : <Feather name='eye' size={24} color='gray' />}
+								</Button>
+							}
+						/>
+					)}
+					name='password'
+				/>
+				{errors.password && <Text>Please enter password</Text>}
+			</View>
 
-      <Pressable
-        textAlign="center"
-        borderWidth="3"
-        borderColor="violet.900"
-        variant="rounded"
-        backgroundColor="violet.900"
-        color="violet.900"
-        p="5"
-        m="2"
-        onPress={handleSubmit(onSubmit)}
-      >
-        <Text textAlign="center" color="white">
-          Log In
-        </Text>
-      </Pressable>
-    </View>
-  );
+			<Button w='93%' alignSelf='center' backgroundColor='violet.800' p='5' m='2' onPress={handleSubmit(onSubmit)}>
+				<Text alignSelf='center' color='white'>
+					Log In
+				</Text>
+			</Button>
+		</KeyboardAvoidingView>
+	)
 }
