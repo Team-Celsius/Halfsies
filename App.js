ee833ab4ac7020ebbf1ad9fc364591f86f81056a
sking11 <114305188+sking11@users.noreply.github.com>
2023-02-12 18:34:40 -0700

    added camera button on relevant pages to take you back to camera view. styled header.

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import 'react-native-gesture-handler'
-import { NativeBaseProvider } from 'native-base'
+import 'intl'
+import 'intl/locale-data/jsonp/en'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { NativeBaseProvider, Button } from 'native-base'
+import CameraView from './Components/Camera/CameraView'
 import LoginPage from './Components/Login/LoginPage'
 import Participants from './Components/Participants'
 import AssignItems from './Components/AssignItems'
-import CameraView from './Components/Camera/CameraView'
 import { onAuthStateChanged } from 'firebase/auth'
+import { FontAwesome } from '@expo/vector-icons'
 import { auth } from './Firebase/firebaseConfig'
-import { useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import 'intl'
-import 'intl/locale-data/jsonp/en'
 import Summary from './Components/Summary'
+import 'react-native-gesture-handler'
+import { useState } from 'react'
+
 const Stack = createNativeStackNavigator()
 
 export default function App() {
@@ -32,9 +34,75 @@ export default function App() {
 				<Stack.Navigator initialRouteName='LogIn'>
 					<Stack.Screen name='LogIn' component={LoginPage} options={{ title: 'Log In', headerShown: false }} />
 					<Stack.Screen name='Camera' component={CameraView} options={{ headerShown: false }} />
-					<Stack.Screen name='Participants' component={Participants} options={{ title: 'Who are we splitting with?' }} />
-					<Stack.Screen name='AssignItems' component={AssignItems} options={{ title: 'Assign Items' }} />
-					<Stack.Screen name='Summary' component={Summary} />
+					<Stack.Screen
+						name='Participants'
+						component={Participants}
+						options={({ navigation }) => ({
+							title: 'Participants',
+							headerStyle: {
+								backgroundColor: '#5B21B6',
+							},
+							headerTintColor: '#fff',
+							headerTitleStyle: {
+								fontWeight: 'bold',
+							},
+							headerRight: () => (
+								<Button
+									onPress={() => {
+										navigation.navigate('Camera')
+									}}
+									bgColor='transparent'>
+									<FontAwesome name='camera-retro' size={30} color='white' />
+								</Button>
+							),
+						})}
+					/>
+					<Stack.Screen
+						name='AssignItems'
+						component={AssignItems}
+						options={({ navigation }) => ({
+							title: 'Assign Items',
+							headerStyle: {
+								backgroundColor: '#5B21B6',
+							},
+							headerTintColor: '#fff',
+							headerTitleStyle: {
+								fontWeight: 'bold',
+							},
+							headerRight: () => (
+								<Button
+									onPress={() => {
+										navigation.navigate('Camera')
+									}}
+									bgColor='transparent'>
+									<FontAwesome name='camera-retro' size={30} color='white' />
+								</Button>
+							),
+						})}
+					/>
+					<Stack.Screen
+						name='Summary'
+						component={Summary}
+						options={({ navigation, route }) => ({
+							title: 'Summary',
+							headerStyle: {
+								backgroundColor: '#5B21B6',
+							},
+							headerTintColor: '#fff',
+							headerTitleStyle: {
+								fontWeight: 'bold',
+							},
+							headerRight: () => (
+								<Button
+									onPress={() => {
+										navigation.navigate('Camera')
+									}}
+									bgColor='transparent'>
+									<FontAwesome name='camera-retro' size={30} color='white' />
+								</Button>
+							),
+						})}
+					/>
 				</Stack.Navigator>
 			</NativeBaseProvider>
 		</NavigationContainer>
